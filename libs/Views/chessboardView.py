e540fbb45d72c090cf08e258032da085cedbbad9
Irakli <greatpersonnot27@gmail.com>
2020-02-29 21:18:41 +0100

    changed the relative path for chessboardView.py and added requirements.txt

--- libs/Views/chessboardView.py
+++ libs/Views/chessboardView.py
@@ -5,17 +5,19 @@ import pygame
 from libs.chessboard import ChessBoard
 from libs.figures import *
 
-path_to_directory = r'libs\Views\sprites'
+path_to_directory = r'sprites'
 WINDOW_HEIGHT = 480
 WINDOW_WIDTH = 480
 FIGURE_SIZE = 60
 SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)
+
 
 def draw_board(board_array, image_dict, screen):
     for row_index, row in enumerate(board_array):
         for col_index, piece in enumerate(row):
             if piece:
                 screen.blit(image_dict[str(piece)], (col_index * FIGURE_SIZE, row_index * FIGURE_SIZE))
+
 
 def load_images(path_to_directory):
     image_dict = {}
@@ -26,22 +28,24 @@ def load_images(path_to_directory):
             image_dict[key] = pygame.image.load(path)
     return image_dict
 
+
 def mouse_handler(positions):
     mouse_position = pygame.mouse.get_pos()
-    row = int(mouse_position[1]/60)
-    column = int(mouse_position[0]/60)
+    row = int(mouse_position[1] / 60)
+    column = int(mouse_position[0] / 60)
     if positions[0]:
         positions[1] = (row, column)
         cb.move(positions[0], positions[1])
         positions[0] = None
     else:
-        positions[0] = (row,column)
+        positions[0] = (row, column)
 
 
+print(os.path.dirname(__file__))
 pygame.init()
 
 SCREEN = pygame.display.set_mode(SIZE)
-BG = pygame.image.load(r'libs\Views\sprites\chessboard.png')
+BG = pygame.image.load(r'sprites\chessboard.png')
 BG = pygame.transform.scale(BG, SIZE)
 
 pieces_image_dict = load_images(path_to_directory)
