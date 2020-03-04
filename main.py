fda2099f6e4c8888579f9d81f38bc2a811d33ac5
Irakli <greatpersonnot27@gmail.com>
2020-03-04 22:15:55 +0100

    added available options dictionary, modified bat file

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import queue
 import sys
 from libs.chessboard import ChessBoard
 from libs.exceptions import InvalidMoveException
+from libs.engineOptions import EngineOptions
 
 
 class GameEngine:
     def __init__(self):
         self.board = None
-        self.options = {}
+        self.options = EngineOptions()
 
     def engine_loop(self):
-        self.initialize()
 
         while True:
             # Check input from GUI
             _input = input()
-            if _input == "isready":
+            if _input == "uci":
+                self.initialize()
+
+            elif _input == "isready":
                 print("readyok")
 
             elif _input.startswith("position"):
@@ -23,7 +26,7 @@ class GameEngine:
 
             elif _input.startswith("go"):
                 self.handle_go(_input)
-            
+
             elif _input == "stop":
                 self.handle_stop()
 
@@ -36,10 +39,9 @@ class GameEngine:
         for move in moves:
             self.board.move_algebra(move)
 
-        #all_moves = self.board.get_all_possible_moves()
-        #for _from, to in all_moves:
+        # all_moves = self.board.get_all_possible_moves()
+        # for _from, to in all_moves:
         #    print(str(self.board.board[_from[0]][_from[1]]), to)
-
 
     def handle_go(self, go):
         if self.board is not None:
@@ -49,13 +51,11 @@ class GameEngine:
         raise NotImplementedError
 
     def initialize(self):
-        if input() == 'uci':
-            print("id name AIengine")
-            print("id author ika&shota")
-            print("uciok")
-            sys.stdout.flush()
-        else:
-            exit(1)
+        print("id name AIengine")
+        print("id author ika&shota")
+        self.options.send_available_options()
+        print("uciok")
+        sys.stdout.flush()
 
 
 if __name__ == '__main__':
